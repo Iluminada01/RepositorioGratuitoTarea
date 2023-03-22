29d881e6035d10d00747b2e2487f13cfacfa3fa2
cabralyo <iluminadayeterna01@gmail.com>
2023-03-22 15:29:39 +0100

    Hemos creado un paquete llamado RamaOsorio y dentro tiene una clase

---
+++ ProyectoAlejandraOsorio/src/RamaOsorio/While1.java
@@ -0,0 +1,20 @@
+package RamaOsorio;
+
+
+
+public class While1 {
+  public static void main(String[] args) {
+	
+        int i=360;
+        
+        
+        while(i<=360 && i>=120){
+      i=i-20;
+       System.out.println(i);
+  }
+        
+  }
+  
+        
+        
+}
